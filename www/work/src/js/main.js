65ad8e23cabcc47d9a493a1d06e58010c739cf4b
Oleh <Bashtovyi>
2016-12-02 01:07:35 +0300

    first commit; rework apollo to gulp-sass

---
+++ www/work/src/js/main.js
@@ -0,0 +1,11 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/owl-carousel/owl-carousel/owl.carousel.js
+
+
+/*
+ * Custom
+ */
+//= app/app.js
